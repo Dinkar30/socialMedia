3cd17e1e3ffb53880de83fdc5ae2f73a7dd8917d
Dinkar30 <dkcan00@gmail.com>
2025-11-06 10:24:31 +0530

    add login functionality and user authentication

--- frontEnd/frontend/src/App.jsx
+++ frontEnd/frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() { 
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+     <BrowserRouter>
+     <Routes>
+      <Route>
+        
+      </Route>
+     </Routes>
+     </BrowserRouter>
     </>
   )
 }
---
+++ frontEnd/frontend/src/pages/login.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import api from "../utils/api";
+
+function Login() {
+   const [id , setId] = useState('')
+   const [password , setPassword] = useState('')
+
+   const handleClick = async (e) => {
+    e.preventDefault()
+
+    try {
+       const response = await api.post('/users/login', {
+        username: id,
+        email: id,
+        password,
+       })
+       // response check 
+       console.log(response.data)
+    
+       localStorage.setItem('accessToken', response.data.data.accessToken)
+       localStorage.setItem('refreshToken', response.data.data.refreshToken)
+       alert('login successful')
+
+    } catch (error) {
+        console.error(error)
+        alert('login failed'+ error.response?.data?.message )
+    }
+    
+   }
+
+   return (
+    <form onSubmit={handleClick}>
+    <input type="text"
+           value={id}
+           placeholder="username/email"
+           onChange={(e) => setId(e.target.value)}
+    />
+    <input type="password"
+    value={password}
+    placeholder="password"
+    onChange={(e) => setPassword(e.target.value)}/>
+    
+    
+    <button type="submit" >Login</button>
+    </form>
+   )
+ }
+
+export default Login
