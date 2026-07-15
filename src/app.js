d19419b7dade0f8cec767c2435f5c9e5e9f354c8
Dinkar30 <dkcan00@gmail.com>
2026-07-15 18:33:31 +0530

    ensure  mandatory gateway

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ import cookieParser from "cookie-parser";
 
 const app = express()
 
-
+app.use((req, res, next) => {
+    // We only allow requests that have the Gateway's Secret Shield Header
+    if (req.headers['x-shield'] !== process.env.SECRET_KEY) {
+        return res.status(403).json({ error: "Access Denied: Direct traffic forbidden." });
+    }
+    next();
+});
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true ,limit: "16kb"}))
